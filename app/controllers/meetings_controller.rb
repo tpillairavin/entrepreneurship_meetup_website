23de4fc6b00305866ce8bf2bf2a6bd3060c62dd8
Ravin <ravin@gocardless.com>
2014-10-02 23:21:56 +0100

    get meetings working and add devise to gemfile

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -4,8 +4,16 @@ class MeetingsController < ApplicationController
   end
 
   def create
+    @meeting = Meeting.new(safe_meeting)
+    @meeting.save
+    redirect_to "/meetings"
   end
 
   def show
   end
+
+  private
+    def safe_meeting
+      params.require('meeting').permit(:location, :start_time, :end_time)
+    end
 end
