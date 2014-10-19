6aaceb9276eedc8201e8ebc1f14b3f89425adcc2
Ravin <ravin@gocardless.com>
2014-10-19 16:41:04 +0100

    remove all user related files

--- app/mailers/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserMailer < ActionMailer::Base
-  default from: "ravin@menschly.com"
-
-  def welcome_email(user)
-    @user=user
-    @url='http://stark-inlet-7537.herokuapp.com/meetings/show'
-    mail(to: @user.email, subject: "Welcome to Entrepreneurship")
-  end
-end
